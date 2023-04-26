97668e991ee83b2c96c779cec87435d9eec2d403
Rajath Thomas Kurian <107932467+rajath-tk@users.noreply.github.com>
2023-04-26 23:12:45 +0530

    Removed API Key

--- Backend/OpenAI_api.py
+++ Backend/OpenAI_api.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-openai.api_key = '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' #replace with api key
+openai.api_key = '' #replace with api key
 
 def getAIresult(type,query):
     response = openai.Completion.create(
